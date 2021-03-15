ab6659a28ff3e8acc781026f60e3ee981965ad46
Keval <keval.langalia@live.com>
2021-03-15 18:22:13 +0530

    removed unnecessary code

--- iBank/ViewController.swift
+++ iBank/ViewController.swift
@@ -12,10 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("akash")
-        print("prachi")
-        print("Hello")
-
     }
 
 
